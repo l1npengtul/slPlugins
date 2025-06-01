95151587f14c5b204747cfd8f51eec7fe213e831
l1npengtul <l1npengtul@protonmail.com>
2025-06-01 15:13:01 +0900

    wuafiwoa

--- 3rdparty/soundpipe/h/thresh.h
+++
@@ -1,9 +0,0 @@
-typedef struct {
-    int mode, init;
-    SPFLOAT prev, thresh;
-} sp_thresh;
-
-int sp_thresh_create(sp_thresh **p);
-int sp_thresh_destroy(sp_thresh **p);
-int sp_thresh_init(sp_data *sp, sp_thresh *p);
-int sp_thresh_compute(sp_data *sp, sp_thresh *p, SPFLOAT *in, SPFLOAT *out);
